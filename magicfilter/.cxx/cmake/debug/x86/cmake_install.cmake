7e24da0b86e95df0b7f71a34436f7eb977967c98
314960314@qq.com <a15158012075>
2023-04-23 14:50:56 +0800

    更新

--- magicfilter/.cxx/cmake/debug/x86/cmake_install.cmake
+++ magicfilter/.cxx/cmake/debug/x86/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/350/magic-camera/magicfilter
+# Install script for directory: D:/350/MagicCamera/magicfilter
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/350/magic-camera/magicfilter/.cxx/cmake/debug/x86/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/350/MagicCamera/magicfilter/.cxx/cmake/debug/x86/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
